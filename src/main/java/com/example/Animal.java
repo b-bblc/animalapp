c598527c0fb0fca2622710852d4199dadbe7df3e
Artur Begichev <artur.begi4ev@gmail.com.>
2025-02-27 00:21:15 +0100

    Add animal class and Main runner

---
+++ src/main/java/com/example/Animal.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String makeSound() {
+        return "Some animal sound";
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("GenericAnimal", 5);
+        System.out.println(animal.getName() + " is " + animal.getAge()
+                + " years old and says: " + animal.makeSound());
     }
 }
